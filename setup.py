92ab6a465c2ce759ff17a9f59d6e9c66a9e3ff59
Kyle Ellrott <kellrott@gmail.com>
2016-08-20 21:40:57 -0700

    Refactoring to use python distutils build

---
+++ setup.py
@@ -0,0 +1,62 @@
+
+import os
+import sys
+from distutils.core import setup
+import subprocess
+from distutils.spawn import find_executable
+
+
+# Find the Protocol Compiler.
+if 'PROTOC' in os.environ and os.path.exists(os.environ['PROTOC']):
+  protoc = os.environ['PROTOC']
+elif os.path.exists("../src/protoc"):
+  protoc = "../src/protoc"
+elif os.path.exists("../src/protoc.exe"):
+  protoc = "../src/protoc.exe"
+elif os.path.exists("../vsprojects/Debug/protoc.exe"):
+  protoc = "../vsprojects/Debug/protoc.exe"
+elif os.path.exists("../vsprojects/Release/protoc.exe"):
+  protoc = "../vsprojects/Release/protoc.exe"
+else:
+  protoc = find_executable("protoc")
+
+
+
+
+def generate_proto(source, require = True):
+  """Invokes the Protocol Compiler to generate a _pb2.py from the given
+  .proto file.  Does nothing if the output already exists and is newer than
+  the input."""
+
+  if not require and not os.path.exists(source):
+    return
+
+  output = source.replace(".proto", "_pb2.py")
+
+  if (not os.path.exists(output) or
+      (os.path.exists(source) and
+       os.path.getmtime(source) > os.path.getmtime(output))):
+    print("Generating %s..." % output)
+
+    if not os.path.exists(source):
+      sys.stderr.write("Can't find required file: %s\n" % source)
+      sys.exit(-1)
+
+    if protoc is None:
+      sys.stderr.write(
+          "protoc is not installed nor found in ../src.  Please compile it "
+          "or install the binary package.\n")
+      sys.exit(-1)
+
+    protoc_command = [ protoc, "-I.", "--python_out=."  , source ]
+    if subprocess.call(protoc_command) != 0:
+      sys.exit(-1)
+
+generate_proto("bmeg/sample.proto")
+
+setup(name='bmeg-schemas',
+      version='0.1',
+      description='BMEG Schema Build',
+      url='https://github.com/bmeg/bmeg-schemas',
+      packages=['python/bmeg'],
+     )
